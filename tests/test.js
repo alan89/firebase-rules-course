7b0519e52c4ac2e55be825c766df837e9d268bf6
“Alan <alan.escobar.martinez@gmail.com>
2022-04-25 15:13:09 -0500

    Adding rules that involves read operations to determine who is able to access some documents through a role parameter

--- tests/test.js
+++ tests/test.js
@@ -5,12 +5,34 @@ const MY_PROJECT_ID = "firebase-rules-course";
 
 describe("Testing Firebase Rules", () => {
 
-	it("Can read documents in the read-only collecction", async() => {
+	/*it("Can read documents in the read-only collecction", async() => {
 		const firestoreDB =  firebase.initializeTestApp({
 				projectId: MY_PROJECT_ID
 		}).firestore();
 		const testDoc = firestoreDB.collection("readonly").doc("testDoc");
 		await firebase.assertSucceeds(testDoc.get());
+	});*/
+
+	it("Can read documents in the posts collection", async() => {
+		const firestoreDB =  firebase.initializeTestApp({
+				projectId: MY_PROJECT_ID,
+				auth: {
+					uid: " Bne8vaQoiB6TsasEP9yW"
+				}
+		}).firestore();
+		const testDoc = firestoreDB.collection("posts").doc("3vi8MCYnq7aEge3LM1Ly");
+		await firebase.assertSucceeds(testDoc.get());
 	});
-	
+
+	it("Can't read documents in the posts collecction", async() => {
+		const firestoreDB =  firebase.initializeTestApp({
+				projectId: MY_PROJECT_ID,
+				auth: {
+					uid: " yRD34ul3fedpUjnYSgu1"
+				}
+		}).firestore();
+		const testDoc = firestoreDB.collection("posts").doc("3vi8MCYnq7aEge3LM1Ly");
+		await firebase.assertFails(testDoc.get());
+	});
+
 });
